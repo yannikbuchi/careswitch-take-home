ee2573b5c15d7240129893a3a3be6b4b775dd164
Yannik Buchi <yannikbuchi@Yanniks-MacBook-Pro.local>
2024-07-10 15:16:24 -0400

    Adds Delete dialogue for users and workspace.

--- src/routes/users/[id]/edit/+page.svelte
+++ src/routes/users/[id]/edit/+page.svelte
@@ -1,11 +1,33 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { Description } from 'formsnap';
+
 	let { data } = $props();
 	let user = data.user;
 	let full_name = user?.first_name + ' ' + user?.last_name;
+
+	let dialogOpen = $state(false);
 </script>
 
 <h1>Edit User</h1>
 <h1>{full_name}</h1>
 
-<Button variant="destructive">Delete User</Button>
+<Dialog.Root>
+	<Dialog.Trigger>
+		<Button on:click={() => (dialogOpen = true)} variant="destructive">+ Delete User</Button>
+	</Dialog.Trigger>
+	<Dialog.Overlay />
+	<Dialog.Content>
+		<Dialog.Title>Confirm Delete User</Dialog.Title>
+		<Dialog.Description
+			>Are you sure you want to delete this user? This action will permanently remove the user from
+			our servers.</Dialog.Description
+		>
+		<Dialog.Footer class="sm:justify-start">
+			<Dialog.Close asChild>
+				<Button type="button" variant="secondary">Delete</Button>
+			</Dialog.Close>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
--- src/routes/workspaces/[id]/edit/+page.svelte
+++ src/routes/workspaces/[id]/edit/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import DialogDescription from '$lib/components/ui/dialog/dialog-description.svelte';
 	let { data } = $props();
 	let workspace = data.workspace;
+
+	let dialogOpen = $state(false);
 </script>
 
 <h1>Edit Workspace</h1>
@@ -9,4 +13,20 @@
 <!-- TO DO: Add Form to edit workspace name and description -->
 <h1>{workspace?.name}</h1>
 
-<Button variant="destructive">Delete Workspace</Button>
+<Dialog.Root>
+	<Dialog.Trigger>
+		<Button on:click={() => (dialogOpen = true)} variant="destructive">Delete Workspace</Button>
+	</Dialog.Trigger>
+	<Dialog.Overlay />
+	<Dialog.Content>
+		<Dialog.Title>Confirm Delete Workspace</Dialog.Title>
+		<DialogDescription
+			>Are you sure you want to delete this workspace? This action is permanent.</DialogDescription
+		>
+		<Dialog.Footer class="sm:justify-start">
+			<Dialog.Close asChild>
+				<Button type="button" variant="secondary">Delete</Button>
+			</Dialog.Close>
+		</Dialog.Footer>
+	</Dialog.Content>
+</Dialog.Root>
