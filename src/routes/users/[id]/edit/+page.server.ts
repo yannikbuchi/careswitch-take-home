9ab75e43cc96746e30e198cedd924b0890d9d788
Yannik Buchi <yannikbuchi@Yanniks-MacBook-Pro.local>
2024-07-10 16:17:23 -0400

    Reconfigured delete user which now correctly redirects to the users page.

--- src/routes/users/[id]/edit/+page.server.ts
+++ src/routes/users/[id]/edit/+page.server.ts
@@ -1,18 +1,25 @@
 import { prisma } from '$lib/server/db';
 import type { Actions } from '@sveltejs/kit';
 import { fail, redirect } from '@sveltejs/kit';
+import { goto } from '$app/navigation';
 
 export const actions: Actions = {
 	deleteUser: async ({ params }) => {
+		let deleteSuccess = false;
+
 		try {
 			await prisma.user.delete({
 				where: {
 					id: params.id
 				}
 			});
-			return redirect(303, '/users');
+			deleteSuccess = true;
 		} catch (error) {
 			return fail(500, { error: 'Failed to delete user' });
+		}
+
+		if (deleteSuccess) {
+			return redirect(303, '/users');
 		}
 	}
 };
